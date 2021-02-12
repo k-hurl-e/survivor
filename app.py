04167aa2f8d8a684d810e40cabe9a522623b3b09
keenan-h <keenanhurley@gmail.com>
2021-02-12 15:46:36 -0500

    start web design

--- app.py
+++ app.py
@@ -1,23 +1,15 @@
 import sqlite3
-from flask import Flask, render_template, g
+from query.queries import *
+from flask import Flask, render_template
 
 app = Flask(__name__)
-DATABASE = 'survivor.db'
-
-def get_db():
-    db = getattr(g, '_database', None)
-    if db is None:
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-
-# write functions that make the queries you want and then parse them and return strings
-
-@app.route("/")
-def home():
-    return render_template('home.html', full_name=full_name, bio=bio, reel=reel, twitter=twitter, instagram=instagram, facebook=facebook, imdb=imdb)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+@app.route("/")
+def home():
+    return render_template('home.html', num_seasons=num_seasons, list_winners=list_winners, winner_age=winner_age, winner_towns=winner_towns, season_list=season_list, tribal_wins=tribal_wins, in_wins=in_wins, total_wins=total_wins)
 
 @app.teardown_appcontext
 def close_connection(exception):
